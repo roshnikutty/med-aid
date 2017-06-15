2f5f4f2a8c0852d3d22e78977ddcf1ea53edc290
Roshni Kutty <roshni.ajkutty@gmail.com>
2017-06-15 11:16:38 -0400

    linked mongodb- through Heroku app

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 //exports.DATABASE_URL = "mongodb://rk:rk@ds143141.mlab.com:43141/node-medical-assist";
 
-exports.DATABASE_URL = process.env.DATABASE_URL ||
+exports.DATABASE_URL = process.env.MONGODB_URI ||
     global.DATABASE_URL || 'mongodb://localhost/node-medical-assist';
 
 exports.PORT = process.env.PORT || 8080;
@@ -9,8 +9,8 @@ exports.PORT = process.env.PORT || 8080;
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
     global.DATABASE_URL || 'mongodb://localhost/test-medical-assist';
 
-exports.AUTH_DATABASE_URL = process.env.AUTH_DATABASE_URL ||
-    global.DATABASE_URL || 'mongodb://localhost/auth-medical-assist';
+// exports.AUTH_DATABASE_URL = process.env.AUTH_DATABASE_URL ||
+//     global.DATABASE_URL || 'mongodb://localhost/auth-medical-assist';
 
 
 exports.JWT = {  
